7f6d7394ad9d64463b1a5c6430747e53ee85b29b
Martins Ojo <martinsojoola2@gmail.com>
2022-12-01 01:18:16 +0100

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10,10)
+
+number = random.randint(-10, 10)
 if number > 0:
-	print(f"{number} is positive\n")
+    print(f"{number} is positive\n")
 elif number == 0:
-	print(f"{number} is zero\n")
+    print(f"{number} is zero\n")
 else:
-	print(f"{number} is negative\n")
+    print(f"{number} is negative\n")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000,10000)
+
+number = random.randint(-10000, 10000)
 number_string = str(number)
-last_digit = number_string[len(number_string)-1]
-if last_digit > '5':
-	print(f"Last digit of {number} is {last_digit} and greater than 5")
-if last_digit == '0':
-	print(f"Last digit of {number} is {last_digit} and is 0")
-if last_digit < '6' and last_digit != 0:
-	print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+last_digit = number_string[len(number_string) - 1]
+if last_digit > "5":
+    print(f"Last digit of {number} is {last_digit} and greater than 5")
+if last_digit == "0":
+    print(f"Last digit of {number} is {last_digit} and is 0")
+if last_digit < "6" and last_digit != 0:
+    print(
+        f"Last digit of {number} is {last_digit} and is \
+     than 6 and not 0"
+    )
