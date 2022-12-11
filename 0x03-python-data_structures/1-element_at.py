f3080d62126374ff4c6e856ec73f979475c6bea6
Martins Ojo <martinsojoola2@gmail.com>
2022-12-11 04:40:11 +0100

    solve task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for item in my_list:
-        if item == my_list[idx]:
-            return item
+    if idx < len(my_list) or idx > 0:
+        for item in my_list:
+            if item == my_list[idx]:
+                return item
+    else:
+        return None
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
