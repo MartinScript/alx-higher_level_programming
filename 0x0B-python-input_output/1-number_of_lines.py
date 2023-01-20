7232dc43579408b2e068550d9c39a529a58011fb
MartinScript <73644503+MartinScript@users.noreply.github.com>
2023-01-20 13:16:11 +0100

    Update 1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3 
- def number_of_lines(filename=""): 
-     count = 0 
-     with open(filename, encoding="utf-8") as fd: 
-         for line in fd: 
-             count += 1 
-     return count
+ """initializate""" 
+  
+  
+ def read_lines(filename="", nb_lines=0): 
+     """ reads n lines of a text file """ 
+     i = len(open(filename).readlines()) 
+     with open(filename, encoding="utf-8") as f: 
+         if nb_lines <= 0 or nb_lines >= i: 
+             line = f.read() 
+             print(line, end="") 
+         else: 
+             for _ in range(nb_lines): 
+                 lines = next(f).strip() 
+                 print(lines) 
+     f.close()
