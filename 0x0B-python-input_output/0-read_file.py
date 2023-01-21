97fe441484384fa6444950577fee589bd2700091
martinsojoola2@gmail.com <martinsojoola2@gmail.com>
2023-01-21 07:04:59 +0100

    fix bugs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3 
- """initialize"""
- 
- 
- def read_file(filename=""): 
-     """create function that read atext file""" 
-     with open(filename, encoding="UTF8") as f: 
-         line = f.read() 
-     print(line, end="") 
-     f.close()
+ """module 0-read_file.py 
+ """ 
+  
+  
+ def read_file(filename=""):
+    """Function reads a text file (UTF8) and prints it to stdout.
+        Args: 
+     filename: file to open 
+     Return: 
+     file contents
+     """ 
+     with open(filename, encoding="utf-8") as f_obj: 
+         file_content = f_obj.read() 
+         print(file_content, end="")
+    
