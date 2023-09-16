7e82b0993f9ab117df0124438a5e9d881f03f94e
MartinScript <martinsojoola2@gmail.com>
2023-09-16 20:31:46 +0100

    fix bugs

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -8,10 +8,3 @@ def remove_char_at(str, n):
     string = string[0:n:] + string[n + 1 :]
 
     return string
-
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
