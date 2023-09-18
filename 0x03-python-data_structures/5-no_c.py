9073f15fe4f9846491f2fa06711b21e16398c1f6
MartinScript <martinsojoola2@gmail.com>
2023-09-19 00:43:02 +0100

    fix bugs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,3 @@
 def no_c(my_string):
     new_string = "".join(c for c in my_string if c != "c" and c != "C")
     return new_string
-
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
