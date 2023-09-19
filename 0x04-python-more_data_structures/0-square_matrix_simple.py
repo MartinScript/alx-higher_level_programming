83a4abbda92b9b42d4b907c893b746ea484c616d
MartinScript <martinsojoola2@gmail.com>
2023-09-19 01:50:24 +0100

    fix bugs

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
 def square_matrix_simple(matrix=[]):
-    square_matrix = list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
+    square_matrix = list(
+        map(lambda row: list(map(lambda x: x * x, row)), matrix))
     return square_matrix
+
+
+matrix = [[]]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,14 +5,11 @@ function that returns a set of all elements present in only one set
 
 
 def only_diff_elements(set_1, set_2):
-    """convert sets into list"""
-    new_set_1 = [x for x in set_1]
-    new_set_2 = [x for x in set_2]
-    """add list"""
-    new_set = new_set_1 + new_set_2
-    for item in new_set:
-        """if item in both set"""
-        if item in new_set_1 and item in new_set_2:
-            """remove item"""
-            new_set.remove(item)
+    new_set = set_1 - set_2
     return new_set
+
+
+set_1 = {"Python", "Javascript"}
+set_2 = {"Python", "Javascript"}
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
