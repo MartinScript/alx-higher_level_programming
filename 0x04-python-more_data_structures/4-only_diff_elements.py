6dbf4b49d667ed953909f023cb2a8b7c5fb8730e
MartinScript <martinsojoola2@gmail.com>
2023-09-24 13:21:28 +0100

    fix bugs

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,11 +5,15 @@ function that returns a set of all elements present in only one set
 
 
 def only_diff_elements(set_1, set_2):
-    new_set = set_1 - set_2
+    """convert sets into list"""
+    new_set_1 = [x for x in set_1]
+    new_set_2 = [x for x in set_2]
+    """add list"""
+    new_set = set(new_set_1 + new_set_2)
     return new_set
 
 
-set_1 = {"Python", "Javascript"}
-set_2 = {"Python", "Javascript"}
+set_1 = {"Python"}
+set_2 = {"Python"}
 od_set = only_diff_elements(set_1, set_2)
 print(sorted(list(od_set)))
