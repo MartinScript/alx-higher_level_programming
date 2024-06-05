2561a20079e77cc8c960fdbd8f47c6a56c1d26db
MartinScript <martinsojoola2@gmail.com>
2024-06-05 09:50:36 +0100

    fix bugs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
+// Import the built-in Node.js 'fs' module.
+
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  // Use fs.readFile() to read the contents of a file specified as a command-line argument
+  // 'utf8' specifies the encoding of the file being read
+
+  if (error) {
+    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
+    console.error('Error reading the file:', error);
+
   } else {
-    console.log(data);
+    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
+    console.log(content);
   }
 });
