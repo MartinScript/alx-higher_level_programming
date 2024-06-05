10022b7be70856a72c9b312fc4025a782da55de4
MartinScript <martinsojoola2@gmail.com>
2024-06-05 10:54:55 +0100

    corrections

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
+// Reading from file
+
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log(data);
-  }
-});
+  });
